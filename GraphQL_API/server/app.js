5d3e8e1305c9b980fce85dde161078fd14286b4d
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-06-30 23:06:59 -0500

    was able to successfully get connected to atlas db. Had to make sure i know the password, created .env file where i keep login details, gitignored it and held the connection string there and passed the env code in the file

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('../schema/schema');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
+const mongoURL = process.env.MONGO_URI;
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
